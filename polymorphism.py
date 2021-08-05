68486c106408088991a1f33e801c858e71b86069
Krupanandhini <73271316+Krupanandhini@users.noreply.github.com>
2021-08-05 20:28:55 +0530

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -1,6 +1,3 @@
-'''Polymorphism is an ability (in OOP) to use a common interface for multiple forms (data types).
-Suppose, we need to color a shape, there are multiple shape options (rectangle, square, circle). However we could use the same method to color any shape. This concept is called Polymorphism.'''
-
 class Parrot:
     def fly(self):
         print("Parrot can fly")
@@ -26,10 +23,6 @@ peggy = Penguin()
 flying_test(blu)
 flying_test(peggy)
 
-Output
+'''Output
 Parrot can fly
-Penguin can't fly
-
-'''In the above program, we defined two classes Parrot and Penguin. Each of them have a common fly() method. However, their functions are different.
-To use polymorphism, we created a common interface i.e flying_test() function that takes any object and calls the object's fly() method. 
-Thus, when we passed the blu and peggy objects in the flying_test() function, it ran effectively.'''
+Penguin can't fly'''
