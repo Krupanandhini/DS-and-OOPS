8704113aa435cd861253bd39550733d40e097336
Krupanandhini <73271316+Krupanandhini@users.noreply.github.com>
2021-08-13 01:22:49 +0530

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,27 +1,47 @@
-class BinaryTreeNode:
-  def __init__(self, data):
-    self.data = data
-    self.leftChild = None
-    self.rightChild = None
-     
-a1=BinaryTreeNode(10)
-a2=BinaryTreeNode(15)
-a3=BinaryTreeNode(20)
+class TreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.children = []
+        self.parent = None
 
-a1.leftChild=a2
-a1.rightChild=a3
+    def add_child(self,child):
+        child.parent =self
+        self.children.append(child)
 
-print("Root Node is:")
-print(a1.data)
-print("left child of node is:")
-print(a1.leftChild.data)
-print("right child of node is:")
-print(a1.rightChild.data)
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level+=1
+            p = p.parent
+        return level
 
-'''output
-Root Node is:
-10
-left child of node is:
-15
-right child of node is:
-20'''
+    def print_tree(self):
+        spaces = ' '*self.get_level()*4
+        prefix = spaces +'* ' if self.parent else ''
+        print(prefix + self.data)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
+
+def build_Vehicle():
+    root = TreeNode('Vehicle')
+
+    car = TreeNode('car')
+    car.add_child(TreeNode('Audi'))
+    car.add_child(TreeNode('BMW'))
+    car.add_child(TreeNode('Ford'))
+
+    bike = TreeNode('Bike')
+    bike.add_child(TreeNode('Honda'))
+    bike.add_child(TreeNode('TVS'))
+    bike.add_child(TreeNode('Royal Enfield'))
+
+    root.add_child(car)
+    root.add_child(bike)
+
+    root.print_tree()
+
+if __name__ =='__main__':
+    build_Vehicle()
+
